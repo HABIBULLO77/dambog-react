d410dec1b6de44a39f03c92e1bec73e90a133138
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-10-02 19:25:47 +0900

    feat: Material UI customized integration

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.css";
+import "./css/index.css";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./app/MaterialTheme";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -12,7 +14,10 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
