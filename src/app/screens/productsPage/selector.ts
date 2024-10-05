d806832f02f42c8e201aa63cdcbe9806b9c3a1bc
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-10-05 15:23:19 +0900

    feat: modified index.tsx in src folder and added other components

---
+++ src/app/screens/productsPage/selector.ts
@@ -0,0 +1,19 @@
+import { createSelector } from "@reduxjs/toolkit";
+import { AppRootState, ProductsPageState } from "../../../lib/types/screen";
+
+const selectProductsPage = (state: AppRootState) => state.productsPage;
+
+export const retrieveRestaurant = createSelector(
+  selectProductsPage,
+  (productsPage: ProductsPageState) => productsPage.restaurant
+);
+
+export const retrieveChosenProduct = createSelector(
+  selectProductsPage,
+  (productsPage: ProductsPageState) => productsPage.chosenProduct
+);
+
+export const retrieveProducts = createSelector(
+  selectProductsPage,
+  (productsPage: ProductsPageState) => productsPage.products
+);
