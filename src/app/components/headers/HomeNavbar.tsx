6be9e2043bfc68bc89f228ed32cefe77fa188cea
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-10-10 23:57:47 +0900

    fix: modified HomeNavbar

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -43,61 +43,71 @@ export default function HomeNavbar(props: HomeNavbarProps) {
     handleCloseLogout,
     handleLogoutRequest,
   } = props;
+
   const { authMember } = useGlobals();
   const [count, setCount] = useState<number>(0);
-  const [value, setvalue] = useState<boolean>(true);
+  const [value, setValue] = useState<boolean>(true);
 
   useEffect(() => {
-    console.log("componentDidMount"); // DATA FETCH
     setCount(count + 1);
-
-    return () => {
-      console.log("componentWillUnmount");
-    };
   }, [value]);
 
   /* HANDLERS */
-
   const buttonHandler = () => {
-    setvalue(!value);
+    setValue(!value);
   };
 
   return (
     <div className="home-navbar">
       <Container className="navbar-container">
-        <Stack className="menu">
-          <Box>
-            <NavLink to="/">
-              <img className="brand-logo" src="/icons/burak.svg" />
-            </NavLink>
-          </Box>
-          <Stack className="links">
-            <Box className={"hover-line"}>
-              <NavLink to="/" activeClassName={"underline"}>
+        <Stack
+          className="menu"
+          justifyContent="space-between"
+          alignItems="center"
+          direction="row"
+        >
+          <NavLink to="/">
+            <img
+              className="brand-logo"
+              src="/icons/dambog.svg"
+              alt="Brand Logo"
+            />
+          </NavLink>
+
+          <Stack
+            direction="row"
+            alignItems="center"
+            spacing={2}
+            className="links"
+          >
+            <Box className="hover-line">
+              <NavLink to="/" activeClassName="underline">
                 Home
               </NavLink>
             </Box>
-            <Box className={"hover-line"}>
-              <NavLink to="/products" activeClassName={"underline"}>
+            <Box className="hover-line">
+              <NavLink to="/products" activeClassName="underline">
                 Products
               </NavLink>
             </Box>
-            {authMember ? (
-              <Box className={"hover-line"}>
-                <NavLink to="/orders" activeClassName={"underline"}>
-                  Orders
-                </NavLink>
-              </Box>
-            ) : null}
-            {authMember ? (
-              <Box className={"hover-line"}>
-                <NavLink to="/member-page" activeClassName={"underline"}>
-                  My Page
-                </NavLink>
-              </Box>
-            ) : null}
-            <Box className={"hover-line"}>
-              <NavLink to="/help" activeClassName={"underline"}>
+
+            {authMember && (
+              <>
+                <Box className="hover-line">
+                  <NavLink to="/orders" activeClassName="underline">
+                    Orders
+                  </NavLink>
+                </Box>
+                <Box className="hover-line">
+                  <NavLink to="/member-page" activeClassName="underline">
+                    My Page
+                  </NavLink>
+                </Box>
+              </>
+            )}
+
+            <Box className="hover-line">
+              <NavLink to="/help" activeClassName="underline">
                 Help
               </NavLink>
             </Box>
@@ -109,16 +119,24 @@ export default function HomeNavbar(props: HomeNavbarProps) {
               onDelete={onDelete}
               onDeleteAll={onDeleteAll}
             />
+
             {!authMember ? (
-              <Box>
+              <Stack direction="row" spacing={2}>
                 <Button
+                  className="signup-button"
                   variant="contained"
+                  onClick={() => setSignupOpen(true)}
+                >
+                  SIGN UP
+                </Button>
+                <Button
                   className="login-button"
+                  variant="contained"
                   onClick={() => setLoginOpen(true)}
                 >
-                  Login
+                  LOGIN
                 </Button>
-              </Box>
+              </Stack>
             ) : (
               <img
                 className="user-avatar"
@@ -127,73 +145,68 @@ export default function HomeNavbar(props: HomeNavbarProps) {
                     ? `${serverApi}/${authMember?.memberImage}`
                     : "/icons/default-user.svg"
                 }
-                aria-haspopup={"true"}
+                aria-haspopup="true"
                 onClick={handleLogoutClick}
+                alt="User Avatar"
               />
             )}
-            <Box className="signup">
-              {!authMember ? (
-                <Button
-                  className={"signup-button"}
-                  variant={"contained"}
-                  onClick={() => setSignupOpen(true)}
-                >
-                  SIGN UP
-                </Button>
-              ) : null}
-            </Box>
-            <Menu
-              anchorEl={anchorEl}
-              id="account-menu"
-              open={Boolean(anchorEl)}
-              onClose={handleCloseLogout}
-              onClick={handleCloseLogout}
-              PaperProps={{
-                elevation: 0,
-                sx: {
-                  overflow: "visible",
-                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                  mt: 1.5,
-                  "& .MuiAvatar-root": {
-                    width: 32,
-                    height: 32,
-                    ml: -0.5,
-                    mr: 1,
-                  },
-                  "&:before": {
-                    content: '""',
-                    display: "block",
-                    position: "absolute",
-                    top: 0,
-                    right: 14,
-                    width: 10,
-                    height: 10,
-                    bgcolor: "background.paper",
-                    transform: "translateY(-50%) rotate(45deg)",
-                    zIndex: 0,
-                  },
+          </Stack>
+
+          <Menu
+            anchorEl={anchorEl}
+            id="account-menu"
+            open={Boolean(anchorEl)}
+            onClose={handleCloseLogout}
+            onClick={handleCloseLogout}
+            PaperProps={{
+              elevation: 0,
+              sx: {
+                overflow: "visible",
+                mt: 1.5,
+                "& .MuiAvatar-root": {
+                  width: 32,
+                  height: 32,
+                  ml: -0.5,
+                  mr: 1,
                 },
-              }}
-              transformOrigin={{ horizontal: "right", vertical: "top" }}
-              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-            >
-              <MenuItem onClick={handleLogoutRequest}>
-                <ListItemIcon>
-                  <Logout fontSize="small" style={{ color: "blue" }} />
-                </ListItemIcon>
-                Logout
-              </MenuItem>
-            </Menu>
-          </Stack>
+                "&:before": {
+                  content: '""',
+                  display: "block",
+                  position: "absolute",
+                  top: 0,
+                  right: 14,
+                  width: 10,
+                  height: 10,
+                  bgcolor: "background.paper",
+                  transform: "translateY(-50%) rotate(45deg)",
+                  zIndex: 0,
+                },
+              },
+            }}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+          >
+            <MenuItem onClick={handleLogoutRequest}>
+              <ListItemIcon>
+                <Logout fontSize="small" />
+              </ListItemIcon>
+              Logout
+            </MenuItem>
+          </Menu>
         </Stack>
-        <Stack className={"header-frame"}>
-          <Stack className={"detail"}>
-            <Box className={"head-main-txt"}>
-              World's Most Delicious Cousine
-            </Box>
-            <Box className={"wel-txt"}>The Choice, not just a choice</Box>
-            <Box className={"service-txt"}>24 hours service</Box>
-          </Stack>
+
+        {/* Centered Text Content */}
+        <Stack
+          className="header-frame"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          spacing={2}
+          mt={4}
+        >
+          <Box className="head-main-txt">World's Most Delicious Cuisine</Box>
+          <Box className="wel-txt">The Choice, not just a choice</Box>
+          <Box className="service-txt">24 hours service</Box>
         </Stack>
       </Container>
     </div>
